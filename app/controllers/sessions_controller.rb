a4f0f53b6932a5d87dec48ea6c3887f29b0755d7
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2023-06-09 23:06:29 +0200

    repeat

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,10 @@
 class SessionsController < ApplicationController
   include ActionController::Cookies
   def create
-    user = User.find_by(email: params[:email])
 
-    if user&.authenticate(params[:password])
-      token = generate_jwt_token(user)
-      render json: { token: }
-    else
-      render json: { error: 'Invalid email or password' }, status: :unauthorized
-    end
+
+  end
   end
 
-  def destroy
-    request.headers['Authorization'].split[1]
-    user = jwt_authentication
-    if user
-      render json: { message: 'User logged out successfully' }, status: :no_content
-    else
-      render json: { error: 'No user is currently logged in' }, status: :unprocessable_entity
-    end
   end
 end
