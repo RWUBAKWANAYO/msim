312cda83cf12ada46af9a099ac0e8b9a001d03e6
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2023-06-13 20:04:25 +0200

    repeat

--- app/workers/course_fetcher_worker.rb
+++ app/workers/course_fetcher_worker.rb
@@ -44,7 +44,8 @@ class CourseFetcherWorker
     existing_course = Course.find_by(id: course_id)
 
     if existing_course
-else
+      puts "Skipping existing course with ID #{course_id}"
+    else
  end
   end
 
