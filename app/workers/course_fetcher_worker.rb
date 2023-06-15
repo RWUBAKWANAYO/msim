5cda482c0469e4bbf9b8d0acbd5c9c4e16235be4
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2023-06-15 22:43:52 +0200

    repeat

--- app/workers/course_fetcher_worker.rb
+++ app/workers/course_fetcher_worker.rb
@@ -39,6 +39,7 @@ class CourseFetcherWorker
   def process_course(course)
     course_id = course['id']
     title = course['attributes']['title']
+    description = course['attributes']['description']
 
 
 
