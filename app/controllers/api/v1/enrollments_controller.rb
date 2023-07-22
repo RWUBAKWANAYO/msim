e08066fa3c8cb3895a2d51d2c85ba60f81054580
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2023-07-22 21:49:06 +0200

    repeat

--- app/controllers/api/v1/enrollments_controller.rb
+++ app/controllers/api/v1/enrollments_controller.rb
@@ -1,26 +1,7 @@
 class Api::V1::EnrollmentsController < ApplicationController
   include AuthenticationHelper
 
-  def create
-    course = Course.find(params[:course_id])
-    if current_user.enrollments.exists?(course:)
-      render json: { message: 'Course already enrolled' }, status: :unprocessable_entity
-    else
-
-      enrollment = Enrollment.new(course:, user: current_user, enrollment_date: Date.today)
-
-      if enrollment.save
-        render json: { message: 'Enrollment successful', enrollment: }, status: :created
-      else
-        render json: { message: 'Failed to enroll', errors: enrollment.errors.full_messages },
-               status: :unprocessable_entity
-      end
-    end
   end
 
-  def index
-    enrollments = current_user.user_enrollments
 
-    render json: { enrollments: }, status: :ok
-  end
 end
