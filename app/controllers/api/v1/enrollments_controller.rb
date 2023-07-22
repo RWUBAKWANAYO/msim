1d37bedc44ac2ac191b256b6ab2b570bed0b3860
RWUBAKWANAYO <rwubakwanayoolivier@gmail.com>
2023-07-22 21:49:15 +0200

    repeat

--- app/controllers/api/v1/enrollments_controller.rb
+++ app/controllers/api/v1/enrollments_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::EnrollmentsController < ApplicationController
 
   def create
     course = Course.find(params[:course_id])
-
+    if current_user.enrollments.exists?(course:)
+  else
 
 
     end
